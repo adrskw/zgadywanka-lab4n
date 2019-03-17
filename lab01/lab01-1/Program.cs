74a5fa29a2a13e5063f104b8851db9e405c4c2e5
adrskw <48623654+adrskw@users.noreply.github.com>
2019-03-17 14:24:43 +0100

    update

---
+++ lab01/lab01-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program na powitanie.");
+
+            Console.Write("Podaj imię: ");
+            string imie = Console.ReadLine();
+
+            Console.Write("Podaj nazwisko: ");
+            string nazwisko = Console.ReadLine();
+
+            Console.WriteLine("Witaj " + imie + " " + nazwisko);
+            Console.WriteLine("Witaj {0} {1}. Czy Pan {0} rzeczywiście nazwywa się {1}?", imie, nazwisko);
+
+            Console.Write("Podaj wiek: ");
+            int wiek = Convert.ToInt32(Console.ReadLine());
+
+            if(wiek < 67)
+            {
+                Console.WriteLine(" do emerytury zostało Ci {0} lat ", 67 - wiek);
+            }
+            else if(wiek < 0)
+            {
+                Console.WriteLine("Błędne dane!");
+            }
+            else
+            {
+                Console.WriteLine(" jesteś emerytem");
+            }
+        }
+    }
+}
